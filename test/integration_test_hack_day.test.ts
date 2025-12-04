ada2844aee2b200bd62cf483cb9c57419170a27c
Emma Hawkins <emma.hawkins@sky.uk>
2025-12-04 11:14:55 +0000

    Initial commit

---
+++ test/integration_test_hack_day.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IntegrationTestHackDay from '../lib/integration_test_hack_day-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/integration_test_hack_day-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IntegrationTestHackDay.IntegrationTestHackDayStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
